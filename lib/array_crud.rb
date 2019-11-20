e2d2a21bcefd957856d0d5202eb4baf456e901a9
Alexander kagerer <kagerer19@gmail.com>
2019-11-20 15:07:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,14 +4,12 @@ def create_an_empty_array
 end
 
 def create_an_array
-  a = ["wow", "i", "am","really"]
+  a = ["1", "2", "3", "4"]
   
 end
 
 def add_element_to_end_of_array(array, element)
- array = ["gosh", "this", "is", "really"]
-array << "arrays"
-end
+    array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
